1b0d0a11969dfbdd119bb64b7b507d8cf1d1151a
kakon4366 <kakonbarman4366@gmail.com>
2022-03-25 23:57:13 +0600

    Responsive product style added

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -6,9 +6,9 @@ const Product = ({product}) => {
     return (
         <div className='product'>
             <img src={img} alt='' />
-            <h2>{name}</h2>
+            <h3>{name}</h3>
             <h4>Price: $<span>{price}</span></h4>
-            <button>Add To Cart</button>
+            <button className='cart-button'>Add To Cart</button>
         </div>
     );
 };
