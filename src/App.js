c69cf53fa1b6fe1558fcdbe0c5f1dfcec83594c1
kakon4366 <kakonbarman4366@gmail.com>
2022-03-25 22:28:04 +0600

    Header, Shop and JSON file added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './Components/Header/Header';
+import Shop from './Components/Shop/Shop';
 
 function App() {
   return (
     <div>
-      <h2>Clear The React Logo and Details</h2>
+      <Header></Header>
+      <Shop></Shop>
     </div>
   );
 }
---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Shop = () => {    
+
+    return (
+        <div>
+            <h3>Shop</h3>
+        </div>
+    );
+};
+
+export default Shop;
