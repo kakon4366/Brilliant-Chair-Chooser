1eefb5b1291e78bf4792cb3cdffa18b754657b0c
kakon4366 <kakonbarman4366@gmail.com>
2022-03-26 02:23:43 +0600

    Choose 1 product bug fixed

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CartProduct from '../CartProduct/CartProduct';
 import './Cart.css';
 
 const Cart = ({cart}) => {
+    const [chooseProduct, setChooseProduct] = useState([]);
 
-    const index = Math.round(Math.random() * (cart.length - 1));
-    console.log(cart[index]);
+    const {name, img} = chooseProduct;
+
+    console.log(cart);
+    
+    const chooseSingleProductHandler = () => {
+        const index = Math.round(Math.random() * (cart.length - 1));
+        const product = cart[index];
+        setChooseProduct(product);
+    }
+
+    console.log(chooseProduct);
 
     return (
         <div className='cart'>
@@ -16,7 +26,12 @@ const Cart = ({cart}) => {
                    cart.map(product => <CartProduct key={product.id} product={product}></CartProduct>)
                }
             </div>
-            <button className='choose-button choose-1'>Choose 1 For Me</button>
+
+            <div className='choose-product'>
+                <img src={img ? img : ''} alt="" />
+                <h3>{name ? name : ''}</h3>
+            </div>
+            <button onClick={cart.length === 0 ? undefined : chooseSingleProductHandler} className='choose-button choose-1'>Choose 1 For Me</button>
             <button className='choose-button choose-again'>Choose Again</button>
 
         </div>
