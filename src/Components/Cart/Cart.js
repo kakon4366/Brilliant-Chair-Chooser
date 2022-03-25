67a8b20889fc9fde722391e240fd4683f5e47f07
kakon4366 <kakonbarman4366@gmail.com>
2022-03-26 00:12:34 +0600

    Cart Button styled

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -4,7 +4,11 @@ import './Cart.css';
 const Cart = () => {
     return (
         <div className='cart'>
-            This is cart components
+            <h3>Selected Chair</h3>
+            <br />
+            <button className='choose-button choose-1'>Choose 1 For Me</button>
+            <button className='choose-button choose-again'>Choose Again</button>
+
         </div>
     );
 };
