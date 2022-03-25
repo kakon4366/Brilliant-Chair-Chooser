b9a08b59922eb7b59ff41c19e378635f3615e20d
kakon4366 <kakonbarman4366@gmail.com>
2022-03-25 23:14:59 +0600

    JSON data loaded success

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Shop.css';
 
-const Shop = () => {    
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    
+    useEffect( () => {
+
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+
+    }, [])
+
+    console.log(products);
 
     return (
-        <div>
-            <h3>Shop</h3>
+        <div className='shop-container'>
+            <div className='product-container'>
+                
+            </div>
+            <div className='cart-container'>
+                This is cart area
+            </div>
         </div>
     );
 };
